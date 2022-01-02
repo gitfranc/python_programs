f4d838729e5d3cd73f3977250435abaa03ef7f0f
franc <caifeiliu521@163.com>
2022-01-02 12:27:00 +0800

    delete the output 7z file for Calculator and AlienInvasion

--- calculator/py_to_exe.bat
+++ calculator/py_to_exe.bat
@@ -5,4 +5,6 @@ set outpath=E:\Users\franc\python\project\%2\%2_%3.7z
 
 pyinstaller -D -w  %1  --add-data  "images;images" -n %2_%3.exe
 
+del  /s/q  E:\Users\franc\python\project\%2\*.7z
+
 7z a -t7z %outpath% .\dist\%2_%3.exe\*
