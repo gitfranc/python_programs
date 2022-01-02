e2c5da58d4a854c21d1d6ad29ce7732686afece3
franc <caifeiliu521@163.com>
2022-01-02 12:01:28 +0800

    pack the 7z by tools of 7z

--- calculator/py_to_exe.bat
+++ calculator/py_to_exe.bat
@@ -1,3 +1,5 @@
 rd /s/q dist
 del *.spec
-pyinstaller -F -w  %1  --add-data  "images;images" -n Calculator_%2.exe
+pyinstaller -D -w  %1  --add-data  "images;images" -n Calculator_%3.exe
+
+7z a -t7z E:\Users\franc\python\project\%2\Calculator_%3.7z .\dist\Calculator_%3.exe\*
